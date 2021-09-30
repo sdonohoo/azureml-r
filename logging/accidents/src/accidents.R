d97374ef86aa01aa79e8cd34e81b9c2e5f25859a
sdonohoo <sdonohoo@gmail.com>
2021-09-29 21:10:23 -0500

    Clean up run_id reference

--- logging/accidents/src/accidents.R
+++ logging/accidents/src/accidents.R
@@ -15,17 +15,21 @@ paste(opt$data_folder)
 # Enable mlflow logging to Azure by setting tracking uri to correct AzureML tracking uri
 mlflow_set_tracking_uri(mlflow_get_azureml_tracking_uri())
 
+# Get AZUREML_RUN_ID to set run id in mlflow calls
+run_id <- Sys.getenv("AZUREML_RUN_ID")
+
 # Test dummy experiment tag
-mlflow_set_tag("test", "tagging" , run_id=Sys.getenv("AZUREML_RUN_ID"))
+mlflow_set_tag("language", "R", run_id = run_id)
 
 accidents <- readRDS(file.path(opt$data_folder, "accidents.Rd"))
 summary(accidents)
 
 # Log dummy parameters
 print("Logging dummy parameters to AzureML experiment using MLflow")
-mlflow_log_param("alpha", 0.5, run_id=Sys.getenv("AZUREML_RUN_ID"))
-mlflow_log_param("lambda", 0.5, run_id=Sys.getenv("AZUREML_RUN_ID"))
+mlflow_log_param("alpha", 0.5, run_id=run_id)
+mlflow_log_param("lambda", 0.5, run_id=run_id)
 
+# Train model
 mod <- glm(dead ~ dvcat + seatbelt + frontal + sex + ageOFocc + yearVeh + airbag  + occRole, family=binomial, data=accidents)
 summary(mod)
 predictions <- factor(ifelse(predict(mod)>0.1, "dead","alive"))
@@ -33,8 +37,9 @@ accuracy <- mean(predictions == accidents$dead)
 
 # Log accuracy to AzureML experiment
 print("Logging accuracy metric to AzureML experiment using MLflow")
-mlflow_log_metric(key="Accuracy", value=accuracy, run_id=Sys.getenv("AZUREML_RUN_ID"))
+mlflow_log_metric(key="Accuracy", value=accuracy, run_id=run_id)
 
+# Save model
 output_dir = "outputs"
 if (!dir.exists(output_dir)){
   dir.create(output_dir)
@@ -44,4 +49,4 @@ message("Model saved")
 
 # Log model to experiment using MLflow
 predictor <- crate(~ predict.glm(mod, as.matrix(.x)), mod)
-#mlflow_log_model(predictor, "accident_model", run_id=Sys.getenv("AZUREML_RUN_ID"))
+#mlflow_log_model(predictor, "accident_model", run_id=run_id)
